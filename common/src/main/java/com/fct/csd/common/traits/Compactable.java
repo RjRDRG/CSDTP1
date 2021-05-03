087890f522c2616781589e6bb9a406f5c0dee6fa
Rodrigo Ribeiro <jr.rodrigo.pt@gmail.com>
2021-05-03 11:00:49 +0100

    pa1 1.02

--- common/src/main/java/com/fct/csd/common/traits/Compactable.java
+++ common/src/main/java/com/fct/csd/common/traits/Compactable.java
@@ -1,7 +1,6 @@
 package com.fct.csd.common.traits;
 
 import java.io.Serializable;
-import java.util.Base64;
 
 import org.apache.commons.lang3.SerializationUtils;
 
@@ -14,16 +13,4 @@ public interface Compactable extends Serializable {
 	static <T> T decompact(byte[] bytes) {
 		return SerializationUtils.deserialize(bytes);
 	}
-	
-	default String stringify() {
-		return Base64.getEncoder().encodeToString(this.compact());
-	}
-	
-	static <T> T unstringify(String string) {
-		return decompact(Base64.getDecoder().decode(string));
- 	}
-
-	static String stringify(byte[] bytes) {
-		return Base64.getEncoder().encodeToString(bytes);
- 	}
 }
